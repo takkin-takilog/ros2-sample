9bf0127dd7102ac596ad6677d7dd4e10e89eabf5
takkin <takkin.takilog@gmail.com>
2020-10-10 20:34:26 +0900

    [update] add qos_profile

--- examples_rclpy/examples_rclpy/topic/publisher.py
+++ examples_rclpy/examples_rclpy/topic/publisher.py
@@ -1,20 +1,25 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy
 from std_msgs.msg import String
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__("minimal_publisher")
+
+        qos_profile = QoSProfile(history=QoSHistoryPolicy.KEEP_ALL,
+                                 reliability=QoSReliabilityPolicy.RELIABLE)
+
         # String型のchatterトピックを送信するpublisherの定義
-        self.publisher = self.create_publisher(String, 'chatter')
+        self.publisher = self.create_publisher(String, "chatter", qos_profile)
         # 送信周期毎にtimer_callbackを呼び出し（送信周期は0.5秒）
         self.timer = self.create_timer(0.5, self.timer_callback)
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World!'
+        msg.data = "Hello World!"
         # chatterトピックにmsgを送信
         self.publisher.publish(msg)
         # msgの中身を標準出力にログ
@@ -32,5 +37,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- examples_rclpy/examples_rclpy/topic/subscriber.py
+++ examples_rclpy/examples_rclpy/topic/subscriber.py
@@ -1,16 +1,23 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy
 from std_msgs.msg import String
 
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__("minimal_subscriber")
+
+        qos_profile = QoSProfile(history=QoSHistoryPolicy.KEEP_ALL,
+                                 reliability=QoSReliabilityPolicy.RELIABLE)
+
         # String型のchatterトピックを受信するsubscriptionの定義
         # （listener_callbackは受信毎に呼び出されるコールバック関数）
-        self.subscription = self.create_subscription(
-            String, 'chatter', self.listener_callback)
+        self.subscription = self.create_subscription(String,
+                                                     "chatter",
+                                                     self.listener_callback,
+                                                     qos_profile)
 
     def listener_callback(self, msg):
         # msgの中身を標準出力にログ
@@ -24,5 +31,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
